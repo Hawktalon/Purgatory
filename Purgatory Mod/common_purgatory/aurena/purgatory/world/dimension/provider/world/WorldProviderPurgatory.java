0b8789b8b8206886f718861d42631e4b91d49c20
Hawktalon <OrcaRider911@aol.com>
2013-08-02 12:48:15 -0700

    Finished world Provider

--- Purgatory Mod/common_purgatory/aurena/purgatory/world/dimension/provider/world/WorldProviderPurgatory.java
+++ Purgatory Mod/common_purgatory/aurena/purgatory/world/dimension/provider/world/WorldProviderPurgatory.java
@@ -1,6 +1,10 @@
 package aurena.purgatory.world.dimension.provider.world;
 
+import aurena.purgatory.world.dimension.DimensionIDMulti;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.world.WorldProvider;
+import net.minecraft.world.chunk.IChunkProvider;
+import net.minecraftforge.common.DimensionManager;
 
 public class WorldProviderPurgatory extends WorldProvider
 {
@@ -17,4 +21,35 @@ public class WorldProviderPurgatory extends WorldProvider
 		return "Purgatory";
 	}
 	
+	public static WorldProvider getProviderForDimension(int id)
+	{
+		return DimensionManager.createProviderFor(DimensionIDMulti.DIMID_1);
+	}
+	
+	public String getWelcomeMessage()
+	{
+		return "Taking your soul to Death's Realm";
+	}
+	
+	public IChunkProvider createChunkGenerator()
+	{
+		return new ChunkProviderPurgatory(worldObj, worldObj.getSeed(), true);
+	}
+	
+	//TODO Enable change in config
+	public boolean canRespawnHere()
+	{
+		return true;
+	}
+	
+	protected String setUserMessage(String par1String)
+	{
+		return "Taking your soul to Death's Realm";
+	}
+	
+	public int getRepawnDimension(EntityPlayerMP player)
+	{
+		return 4;
+	}
 }
+	
